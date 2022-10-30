4ebb3c9c722fc576f6c0d84f917edff6bf738109
Omeiza Alabi <omeizaalabi@gmail.com>
2022-10-30 19:50:07 +0100

    Add gameOutcome to GameEnd component

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 export const HAND = [0, 1, 2, 3, 4, 5];
 export const GUESS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-export const GAME_OUTCOME = ['Alice wins', 'Playing', 'Bob Wins'];
+export const GAME_OUTCOME = ['Alice wins game', 'Playing', 'Bob Wins game'];
 export const ROUND_OUTCOME = ['Alice won last round', 'Draw', 'Bob won last round'];
 export const defaults = { defaultFundAmt: 100, defaultWager: 1 }
--- views/player-views/game-end.jsx
+++ views/player-views/game-end.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { GAME_OUTCOME } from '../../utils/constants';
 
-const GameEnd = ({ points }) => {
+const GameEnd = ({ points, gameOutcome }) => {
     const { aliceWinCount, bobWinCount } = points;
     return (
         <div>
             <h3>Game Over</h3>
 
-            <span>{`Alice ${aliceWinCount} : ${bobWinCount} Bob`}</span>
+            <div><span>{`Alice ${aliceWinCount} : ${bobWinCount} Bob`}</span></div>
+            <div>
+                <span>{GAME_OUTCOME[gameOutcome]}</span>
+            </div>
             <button
                 onClick={() => location.href = '/'}
             >Exit</button>
